a62889b97af73c21b6e33720df24758e04c217e1
alisson <alissonmarcos249@gmail.com>
2026-06-14 15:18:00 -0300

    log what are installing

--- p3/scripts/install_dependencies.sh
+++ p3/scripts/install_dependencies.sh
@@ -5,14 +5,12 @@ if command -v docker &>/dev/null && command -v k3d &>/dev/null; then
   exit 0
 fi
 
-# Add Docker's official GPG key:
+printf "\t\t ---- Installing docker ----\n"
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
@@ -21,18 +19,21 @@ Components: stable
 Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
-
 sudo apt update
-
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 sudo usermod -aG docker $USER
 
+
+printf "\t\t ---- Installing kubectl ----\n"
 curl -LO "https://dl.k8s.io/release/v1.36.1/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
+
+printf "\t\t ---- Installing k3d ----\n"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.8.3 bash
 
+
+printf "\t\t ---- Installing argocd ----\n"
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/v3.4.3/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
