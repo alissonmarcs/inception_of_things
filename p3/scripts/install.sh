937e3f190bce2a487111813c915bcc2da547e965
vinicius <niviciusdev@gmail.com>
2026-07-04 17:36:07 -0300

    feat: add install.sh with commented steps to be done.

---
+++ p3/scripts/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# call install_dependencies.sh
+# create argocd and dev namespace
+# check if bonus variable is set to true, if true, call script to deploy gitlab on vm and then deploy applications
+# search for needed steps.
