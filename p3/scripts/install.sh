245b0042aa686743d4d247833394db896f599134
vinicius <niviciusdev@gmail.com>
2026-07-04 23:59:28 -0300

    feat: add all steps needed to run k3d, argocd and application in install.sh

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,6 +1,49 @@
 #!/usr/bin/bash
 
-# call install_dependencies.sh
+export YELLOW="\001\033[1;33m\002" RESET="\001\033[0m\002"
+PORT=9000
+
+logger() {
+    printf "$YELLOW$1$RESET\n"
+}
+
+# call script to check and install dependencies
+./install_dependencies.sh
+
+# create k3d cluster and point redirect traffic from port 8888 to internal port 80 (Traefik load balancer)
+logger "Creating k3d cluster"
+k3d cluster create vde-frei --port "8888:80@loadbalancer"
+
 # create argocd and dev namespace
-# check if bonus variable is set to true, if true, call script to deploy gitlab on vm and then deploy applications
-# search for needed steps.
+logger "Creating argocd and dev namespace"
+kubectl create namespace argocd
+kubectl create namespace dev
+
+logger "Getting argocd secret..."
+export ARGOCD_SECRET=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo "Argocd secret fetched and stored in ARGOCD_SECRET in your environment."
+
+logger "Port-forwarding argocd to $PORT"
+PID_ARGOCD=$(kubectl port-forward -n argocd service/argocd-server $PORT:443 & echo $!)
+echo "Argocd port-forward PID: $PID_ARGOCD"
+
+logger "Logging in to argocd"
+argocd login localhost:$PORT --username admin --password $ARGOCD_SECRET
+
+APP_PORT=4242
+logger "Creating will-playground app"
+argocd app create will-playground --file confs/will42.yaml
+
+logger "Port-forwarding dev to $APP_PORT"
+PID_APP=$(kubectl port-forward -n dev service/will-playground $APP_PORT:8888 & echo $!)
+echo "Dev port-forward PID: $PID_APP"
+
+logger "Waiting for app to be ready"
+argocd app wait will-playground --timeout 30s
+
+logger "App is ready"
+
+logger "Opening browser to argocd"
+open "https://localhost:$APP_PORT"
+
+# TODO: Validate script in macos and linux on vm.
