ce583708523893f864c6a16fee81bf652883b0cd
Alisson <alissonmarcos249@gmail.com>
2026-07-18 16:52:32 -0300

    remocao pasta

--- bonus/confs/demo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-buckets=("git-lfs" "gitlab-agent-plan-content" "gitlab-artifacts" "gitlab-backups" \
-         "gitlab-ci-secure-files" "gitlab-dependency-proxy" "gitlab-mr-diffs" \
-         "gitlab-packages" "gitlab-pages" "gitlab-terraform-state" "gitlab-uploads" \
-         "registry" "runner-cache" "tmp" )
-for bucket in "${buckets[@]}"; do
-  kubectl exec -n garage pod/garage-0  -- /garage bucket allow --read --write --key gitlab-app-key "${bucket}";
-done
